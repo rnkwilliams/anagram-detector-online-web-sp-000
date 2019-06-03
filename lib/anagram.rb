5385f0389f75a1fce8b6520d13061101800bac9b
Ranika Williams <rnkprice07@gmail.com>
2019-06-03 17:41:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,11 +2,14 @@
 def Anagram
 
   attr_accessor :word
+  
   def initialize(word)
     @word = word
   end
 
-  def match
-    word.match
+  def match(array)
+    array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
   end
 end
